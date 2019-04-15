fc6369d71080ad21473f6b8f5c2e70bbfdf59cdd
Jennifer Grace <jennifergrace@Jennifers-MacBook-Pro.local>
2019-04-15 16:58:56 -0400

    some updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// div elements
+var heroDiv = $("#hero");
+var oponentDiv = $("#oponent");
+var lukeDiv = $("#luke");
+var reyDiv = $("#rey");
+var hanSoloDiv = $("#han-solo");
+var yodaDiv = $("#yoda");
+var darthVaderDiv = $("#darth-vader");
+var kyloRenDiv = $("#kylo-ren");
+var darthMaulDiv = $("#darth-maul");
+var senatorPalpatineDiv = $("#senator-palpatine");
